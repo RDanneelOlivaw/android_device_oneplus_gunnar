e21e9eb2ae5e4ed49b8acadb9b2734a460dcf8f4
Albert Tang <tangalbert919@yahoo.com>
2024-06-21 10:05:18 -0500

    gunnar: Update from OOS 11.C.22
    Change-Id: I801d28cf7297e3f4149c5629c1c6e4bcf58411f5

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -11,8 +11,8 @@
 
 using android::base::GetProperty;
 
-constexpr const char* BUILD_DESCRIPTION = "GN2200-user 12 RKQ1.211119.001 17c062b_2_1 release-keys";
-constexpr const char* BUILD_FINGERPRINT = "OnePlus/GN2200/OP515AL1:12/RKQ1.211119.001/17c062b_2_1:user/release-keys";
+constexpr const char* BUILD_DESCRIPTION = "GN2200-user 12 RKQ1.211119.001 1863297-1adfc-a2c6b release-keys";
+constexpr const char* BUILD_FINGERPRINT = "OnePlus/GN2200/OP515AL1:12/RKQ1.211119.001/1863297-1adfc-a2c6b:user/release-keys";
 
 constexpr const char* RO_PROP_SOURCES[] = {
     nullptr,
