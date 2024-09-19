1474801ea85739b2285c68fd99ea38c5791308b3
Albert Tang <tangalbert919@yahoo.com>
2024-09-18 23:34:17 -0500

    gunnar: Decommonize sensors blob fixup
    Change-Id: I4b2cf2bd07295dd086fb84da44fb46dfafe4df00

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/sensors.ssc.so)
+            [ "$2" = "" ] && return 0
+            sed -i "s/qti.sensor.wise_light/android.sensor.light\x00/" "${2}"
+            "${SIGSCAN}" -p "EA D3 84 52 01 41 00 91 29 00 15 CB 29 41 00 D1 29 15 C9 93 4A 3F A0 72" -P "AA 00 80 52 01 41 00 91 29 00 15 CB 29 41 00 D1 29 15 C9 93 0A 00 A0 72" -f "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
