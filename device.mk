1bf71c8d189b4a34d51e9eae23d6dba633aab5c7
Daniel <daniel.rosania1638@gmail.com>
2025-02-04 21:17:10 -0500

    gunnar: remove OMX  * OMX HW video codecs are no longer used after enabling Codec2  * OMX HW audio codecs won't work without QCOM A/V enhancements, which    we don't have on Lineage.
    Change-Id: I65852151c8fb6387add5fc4402ba3f13c97b5f64

--- device.mk
+++ device.mk
@@ -345,16 +345,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# OMX
-PRODUCT_PACKAGES += \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
-    libstagefrighthw
-
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
